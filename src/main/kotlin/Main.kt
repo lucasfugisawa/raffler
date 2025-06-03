a49d7172cac76fa16cba65ed619ae57438f5dd9e
Lucas Fugisawa <lucasfugisawa@gmail.com>
2025-06-03 19:42:53 -0300

    Add progress bar simulation to raffle announcement in `Main.kt`.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,12 +20,31 @@ fun randomInt(start: Int, end: Int): Int {
     return random.nextInt(end - start + 1) + start
 }
 
+/**
+ * Displays a progress bar in the console to simulate calculation.
+ *
+ * @param totalSteps The total number of steps in the progress bar
+ * @param delayMs The delay between steps in milliseconds
+ */
+fun showProgressBar(totalSteps: Int = 30, delayMs: Int = 80) {
+    println("And the JetBrains IDE license goes to...")
+    print("Generating: [")
+    for (i in 1..totalSteps) {
+        print("=")
+        System.out.flush()
+        Thread.sleep(delayMs.toLong())
+    }
+    println("] Done!")
+}
+
 fun main() {
-    // Example usage of the randomizer
     val min = 1
     val max = 100
 
-    println("Generating a random number between $min and $max:")
+    println("Raffle range: [$min..$max]")
+
+    showProgressBar()
+
     val randomNumber = randomInt(min, max)
-    println("Random number: $randomNumber")
+    println("... the lucky person in row: $randomNumber 🎉🎉🎉")
 }
