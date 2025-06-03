8953f3f1471a3d284bd1edd32a1e3b17b1231105
Lucas Fugisawa <lucasfugisawa@gmail.com>
2025-06-03 19:43:57 -0300

    Rename `showProgressBar` to `showFakeButCoolProgressBar` for better readability and fun appeal.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,7 +26,7 @@ fun randomInt(start: Int, end: Int): Int {
  * @param totalSteps The total number of steps in the progress bar
  * @param delayMs The delay between steps in milliseconds
  */
-fun showProgressBar(totalSteps: Int = 30, delayMs: Int = 80) {
+fun showFakeButCoolProgressBar(totalSteps: Int = 30, delayMs: Int = 80) {
     println("And the JetBrains IDE license goes to...")
     print("Generating: [")
     for (i in 1..totalSteps) {
@@ -43,7 +43,7 @@ fun main() {
 
     println("Raffle range: [$min..$max]")
 
-    showProgressBar()
+    showFakeButCoolProgressBar()
 
     val randomNumber = randomInt(min, max)
     println("... the lucky person in row: $randomNumber 🎉🎉🎉")
